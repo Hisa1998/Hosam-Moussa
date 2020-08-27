7e09b9c557e3b2630c9c4eebb530e09e86748758
Hisa1998 <48384235+Hisa1998@users.noreply.github.com>
2020-08-27 20:16:20 +0200

    s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileModule } from "./profile-edit/profile.module";
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 
 const routes: Routes = [
   {
-    path:"me", loadChildren: () => ProfileModule
+    path:"me", component:ProfileEditComponent
+    //loadChildren: () => ProfileModule
   },
   {
     path : '',component:HomeComponent
